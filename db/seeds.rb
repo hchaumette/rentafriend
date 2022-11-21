00986ea2a43c4d4b1c89e72c7283132e6b1afaba
lucas regnier <lucas.regniergithub@gmail.com>
2022-11-21 15:08:08 +0100

    generated users and activites with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,24 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
+puts "seed started"
 
-5.times { User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name) }
+puts "create users"
+user1 = User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name)
+user2 = User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name)
+user3 = User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name)
+user4 = User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name)
+user5 = User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name)
+puts " ended create users"
 
 
-
-Booking.create!()
-
+ Booking.create!()
 
 
-5.times { Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name) }
+puts "create activities"
+activity1 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user1)
+activity2 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user2)
+activity3 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user3)
+activity4 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user4)
+activity5 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user5)
+puts " ended create activites"
