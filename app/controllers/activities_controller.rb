e9224a7cae6555010c89b300cff55678b963971a
Theo Mahoudo <theo.mah@hotmail.com>
2022-11-21 15:38:09 +0100

    Controller activity sans destroy + form

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,24 @@
+class ActivitiesController < ApplicationController
+  def new
+    @activity = Activity.new
+  end
+
+  def index
+    @activities = Activity.all
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to  activities_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :content, :location)
+  end
+end
