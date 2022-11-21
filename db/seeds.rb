e6c4e7560b1f43140b5bdf8eeb975adf50faef4b
lucas regnier <lucas.regniergithub@gmail.com>
2022-11-21 15:18:15 +0100

    added booking seed, finished seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,6 @@ user5 = User.create!(email: Faker::Internet.unique.email, password: "123456", na
 puts " ended create users"
 
 
- Booking.create!()
-
-
 puts "create activities"
 activity1 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user1)
 activity2 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user2)
@@ -27,3 +24,11 @@ activity3 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem
 activity4 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user4)
 activity5 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user5)
 puts " ended create activites"
+
+puts "create booking"
+ booking1 = Booking.create!(user: user1, activity: activity4, state: "pending", start_date: "03/02/2023", end_date: "10/02/2023")
+ booking1 = Booking.create!(user: user5, activity: activity1, state: "accepted", start_date: "03/12/2022", end_date: "10/12/2022")
+ booking3 = Booking.create!(user: user4, activity: activity2, state: "pending", start_date: "23/11/2022", end_date: "10/01/2023")
+ booking4 = Booking.create!(user: user2, activity: activity3, state: "pending", start_date: "19/12/2022", end_date: "10/01/2023")
+ booking5 = Booking.create!(user: user3, activity: activity5, state: "pending", start_date: "03/12/2022", end_date: "04/12/2022")
+puts "ended create booking"
