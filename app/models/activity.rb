634078f80718ae8fc725da4e7e1fcd2c9b201c4d
hugo chaumette <hugochaumette@gmail.com>
2022-11-22 17:07:03 +0100

    newActivity : added a clicable card to select category

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,6 @@
 class Activity < ApplicationRecord
   AVAILABLE_CATEGORIES = ["sport", "games", "party", "nature", "culture"]
+  has_one_attached :photo
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
