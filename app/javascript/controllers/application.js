ac940ec1277b91bb418537b1da4e8618d942cd2b
hugo chaumette <hugochaumette@gmail.com>
2022-11-23 11:15:23 +0100

    removed duplicated imports from application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,19 +7,3 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 export { application }
-
-import { Application } from 'stimulus'
-import { definitionsFromContext } from 'stimulus/webpack-helpers'
-
-const application = Application.start()
-const context = require.context('../controllers', true, /\.js$/)
-application.load(definitionsFromContext(context))
-
-// import Flatpickr
-import Flatpickr from 'stimulus-flatpickr'
-
-// Import style for flatpickr
-require("flatpickr/dist/flatpickr.css")
-
-// Manually register Flatpickr as a stimulus controller
-application.register('flatpickr', Flatpickr)
