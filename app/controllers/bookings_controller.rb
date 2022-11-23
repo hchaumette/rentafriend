edebac176759a251dbb858772e3ec6318a02ccd7
hugo chaumette <hugochaumette@gmail.com>
2022-11-23 12:38:40 +0100

    booking creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,11 @@
 class BookingsController < ApplicationController
 
-  def index
-    @bookings = policy_scope(Booking)
-  end
-
-  def show
-    @bookings = Booking.find(params[:id])
-    authorize @booking
-  end
-
   def create
-
+    @activity = Activity.find(params[:activity_id])
+    @booking = Booking.new(user: current_user, activity: @activity)
+    authorize @booking
+    if @booking.save
+      redirect_to activity_path(@activity), flash: { notice: "You booked this activity!" }
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,6 @@ Booking.destroy_all
 Activity.destroy_all
 User.destroy_all
 
-
 puts "seed started"
 
 puts "create users"
@@ -21,7 +20,6 @@ user3 = User.create!(email: Faker::Internet.unique.email, password: "123456", na
 user4 = User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name, avatar:  "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60")
 user5 = User.create!(email: Faker::Internet.unique.email, password: "123456", name: Faker::Name.unique.name, avatar:  "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60")
 puts " ended create users"
-
 
 puts "create activities"
 activity1 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem.paragraph, location: Faker::FunnyName.name, user: user1, image_url:  "https://images.unsplash.com/photo-1660745878236-c1ae062489b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8Qm4tRGpyY0Jyd298fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60", date:"03/02/2023")
@@ -36,9 +34,9 @@ activity5 = Activity.create!(title: Faker::Hobby.activity, content: Faker::Lorem
 puts " ended create activites"
 
 puts "create booking"
- booking1 = Booking.create!(user: user1, activity: activity3, state: "pending")
- booking1 = Booking.create!(user: user5, activity: activity1, state: "accepted")
- booking3 = Booking.create!(user: user4, activity: activity2, state: "declined")
- booking4 = Booking.create!(user: user2, activity: activity4, state: "accepted")
- booking5 = Booking.create!(user: user1, activity: activity5, state: "accepted")
+Booking.create!(user: user1, activity: activity3, state: "pending")
+Booking.create!(user: user5, activity: activity1, state: "accepted")
+Booking.create!(user: user4, activity: activity2, state: "declined")
+Booking.create!(user: user2, activity: activity4, state: "accepted")
+Booking.create!(user: user1, activity: activity5, state: "accepted")
 puts "ended create booking"
